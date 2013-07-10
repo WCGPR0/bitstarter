bd9a4d5878a570fdb3f2f0a8bb16e0c4416d112c
Victor Wu <victor_moove@hotmail.com>
2013-07-10 17:01:54 +0000

    Dumps the content instead

--- web.js
+++ web.js
@@ -5,9 +5,14 @@ var fs = require('fs');
 var buffer = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer);
-
+  response.send("Connected");
 });
+
+http.createServer(function(request, response){
+    response.writeHeader(200, {"Content-Type": "text/html"});
+    response.write(buffer);
+    response.end();
+}).listen(8000);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
