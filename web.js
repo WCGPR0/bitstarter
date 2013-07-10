c7493410b603d2460719da9476c258241fc58df5
Victor Wu <victor_moove@hotmail.com>
2013-07-10 17:12:08 +0000

    Reverted back

--- web.js
+++ web.js
@@ -5,14 +5,9 @@ var fs = require('fs');
 var buffer = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send("Connected");
+  response.wite(buffer);
 });
 
-http.createServer(function(request, response){
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write(buffer);
-    response.end();
-}).listen(8000);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
