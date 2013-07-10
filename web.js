501b0ac42f4baf15846136cf57b2657717f30c15
Victor Wu <victor_moove@hotmail.com>
2013-07-10 14:04:14 +0000

    First Commit

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFile(index.html));
+var buffer = new Buffer("test");
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8');
+  response.send(buffer.toString('utf-8'));
+
 });
 
 var port = process.env.PORT || 5000;
