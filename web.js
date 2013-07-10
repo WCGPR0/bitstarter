7379addc10fdc4873e22e3881fbbc7f4e669a275
Victor Wu <victor_moove@hotmail.com>
2013-07-10 16:22:52 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var  express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var buffer = fs.readFileSync('./index.html','utf8');
+var buffer = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8'));
+  response.send(buffer);
 
 });
 
